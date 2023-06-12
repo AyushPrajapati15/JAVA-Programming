f10dc463b9eefa32213cdfdf4e1a2115eff75904
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-06-12 22:57:59 +0530

    Day-48 Constructor in java

---
+++ Day-48/src/O1_Cons.java
@@ -0,0 +1,11 @@
+//this is default constructor
+class DefCons{
+    DefCons(){
+        System.out.println("This is default constructor");
+    }
+}
+public class O1_Cons {
+    public static void main(String[] args) {
+        DefCons obj1=new DefCons();
+    }
+}
