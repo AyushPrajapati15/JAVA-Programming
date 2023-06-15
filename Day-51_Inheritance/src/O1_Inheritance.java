4ceee301d27b309d0104b01baca67a3a15dfd1c2
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-06-15 19:30:37 +0530

    Day-51 inheritance

---
+++ Day-51_Inheritance/src/O1_Inheritance.java
@@ -0,0 +1,47 @@
+// Shape class
+class Shape {
+    public double calculateArea() {
+        return 0.0;
+    }
+}
+
+// Rectangle class
+class Rectangle extends Shape {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double calculateArea() {
+        return length * width;
+    }
+}
+
+// Circle class
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+// Main class to demonstrate the usage
+public class O1_Inheritance {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(4.0, 5.0);
+        System.out.println("Area of Rectangle: " + rectangle.calculateArea());
+
+        Circle circle = new Circle(3.0);
+        System.out.println("Area of Circle: " + circle.calculateArea());
+    }
+}
