fa6297ecd51fd8459ed789551a87200054478ee4
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-06-29 23:45:01 +0530

    Day-65 Abstract class

---
+++ Day-65/src/O2_Abstract_Program.java
@@ -0,0 +1,62 @@
+abstract class Shape {
+    public abstract double calculateArea();
+}
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle extends Shape {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double calculateArea() {
+        return length * width;
+    }
+}
+
+class Triangle extends Shape {
+    private double base;
+    private double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    public double calculateArea() {
+        return 0.5 * base * height;
+    }
+}
+
+public class O2_Abstract_Program {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0);
+        Rectangle rectangle = new Rectangle(4.0, 6.0);
+        Triangle triangle = new Triangle(3.0, 8.0);
+
+        double circleArea = circle.calculateArea();
+        double rectangleArea = rectangle.calculateArea();
+        double triangleArea = triangle.calculateArea();
+
+        System.out.println("Area of circle: " + circleArea);
+        System.out.println("Area of rectangle: " + rectangleArea);
+        System.out.println("Area of triangle: " + triangleArea);
+    }
+}
