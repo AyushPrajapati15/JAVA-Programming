d9c0217203ef775a8e62d4c0a238ea6ac77ce3c0
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-07-03 22:36:15 +0530

    Day_69 interface

--- Day-69/src/O1_interface.java
+++ Day-69/src/O1_interface.java
@@ -67,7 +67,7 @@ class Triangle implements Shape {
     }
 }
 
-public class Main {
+public class O1_interface {
     public static void main(String[] args) {
         Circle circle = new Circle(5);
         Rectangle rectangle = new Rectangle(4, 6);
