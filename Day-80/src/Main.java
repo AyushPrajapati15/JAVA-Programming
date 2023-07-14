76206be9cc6985958011a0734804e7bfb7bd4274
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-07-14 23:31:37 +0530

    Day-80 Polymorphism program

---
+++ Day-80/src/Main.java
@@ -0,0 +1,47 @@
+abstract class Shape {
+    abstract double calculateArea();
+}
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+
+class Rectangle extends Shape {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    double calculateArea() {
+        return length * width;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Shape[] shapes = new Shape[3];
+        shapes[0] = new Circle(5.0);
+        shapes[1] = new Rectangle(4.0, 6.0);
+        shapes[2] = new Circle(3.0);
+
+        for (Shape shape : shapes) {
+            double area = shape.calculateArea();
+            System.out.println("Area: " + area);
+        }
+    }
+}
