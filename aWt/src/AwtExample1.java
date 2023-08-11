d45c77265f092ff549e554323406b552dc75fe14
AyushPrajapati15 <aayushprajapati867@gmail.com>
2023-08-11 19:53:26 +0530

    AWT in java

---
+++ aWt/src/AwtExample1.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+public class AwtExample1 {
+    public AwtExample1(){
+//        Create a frame
+        Frame f=new Frame();
+        f.setVisible(true);
+        f.setSize(400,400);
+
+
+
+    }
+    public static void main(String[] args) {
+        AwtExample1 a=new AwtExample1();
+    }
+}
